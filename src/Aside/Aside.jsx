efe8a5a9b9b60018bd75e6d015489eacbabb227a
Sergey_js15 <serj.777.front@gmail.com>
2023-11-22 12:04:01 +0200

    add Component Map

--- src/Aside/Aside.jsx
+++ src/Aside/Aside.jsx
@@ -1,10 +1,12 @@
 import './Aside.css';
+import Map from '../Map/Map'
 
 export const Aside = () => {
 
     return(
         <aside>
             <h2>aside</h2>
+            < Map />
         </aside>
     )
 }
---
+++ src/Map/Map.jsx
@@ -0,0 +1,12 @@
+import './Map.css'
+// https://places.googleapis.com/v1/NAME/media?key=API_KEY&PARAMETERS
+// NAME содержит имя ресурса фотографии.
+// API_KEY содержит ключ API.
+// PARAMETERS содержит параметр maxHeightPx , параметр maxWidthPx или оба параметра.
+const Map = () => {
+  return(
+    <div className="block-image"></div>
+  )
+}
+
+export default Map;
