21b5c640bd63846819714d8c98d3b11bfb37ddd2
Sergey_js15 <serj.777.front@gmail.com>
2023-11-27 16:32:00 +0200

    add change day 3 5 7

--- src/WeatherBlock/WeatherBlock.jsx
+++ src/WeatherBlock/WeatherBlock.jsx
@@ -7,6 +7,18 @@ import { date } from '../utils/date';
 export const WeatherBlock = () => {
     const { data } = useContext(WeatherContext);
     const [stateSeason, setStateSeason] = useState('');
+    const [changeNumberDays, setChangeNumberDays] = useState(3)
+
+    const changeShowTreeDays = () => {
+        setChangeNumberDays(3)
+    }
+    const changeShowFiveDays = () => {
+        setChangeNumberDays(5)
+    }
+    const changeShowSevenDays = () => {
+        setChangeNumberDays(7)
+    }
+    
     useEffect(() => {
         setStateSeason(date.season)
     }, [])
@@ -15,41 +27,42 @@ export const WeatherBlock = () => {
     const arrNameWeek = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"];
 
     if (!data || !data.forecast || !data.forecast.forecastday) {
-        const forecastDays = Array.from({ length: 5 }, (_, index) => ({ index }));
+        const forecastDays = Array.from({ length: changeNumberDays }, (_, index) => ({ index }));
         return (
             <main className={stateSeason}>
-            <div className="block-cards">
-
-                {forecastDays.map((dayData, index) => (
-                    <div className={`cards${index}`} key={index}>
-                        {index === 0 ? <span className='today'>Cьогодні</span> : null}
-                        <img src={'https://i.gifer.com/VAyR.gif'} alt="Image weather" />
-                    </div>
-                ))}
-
-            </div>
-            <div className="weather-info-block">
-                < WeatherTable />
-            </div>
-        </main>
+                <div className="block-cards">
+                    {forecastDays.map((dayData, index) => (
+                        <div className={`cards${index}`} key={index}>
+                            {index === 0 ? <span className='today'>Cьогодні</span> : null}
+                            <img className='loading' src={'https://i.gifer.com/VAyR.gif'} alt="Loading image weather" />
+                        </div>
+                    ))}
+                </div>
+                <div className="weather-info-block">
+                    < WeatherTable />
+                </div>
+            </main>
         )
 
     }
 
-    const forecastDays = data.forecast.forecastday.slice(0, 5);
+    const forecastDays = data.forecast.forecastday.slice(0, changeNumberDays);
 
     return (
         <main className={stateSeason}>
-            <div className="block-cards">
+            <button className='btn-changeShowThree' onClick={changeShowTreeDays}>3 дні</button>
+            <button className='btn-changeShowFive' onClick={changeShowFiveDays}>5 днів</button>
+            <button className='btn-changeShowSeven' onClick={changeShowSevenDays}>7 днів</button>
 
+            <div className="block-cards">
                 {forecastDays.map((dayData, index) => (
                     <div className={`cards${index}`} key={index}>
                         {index === 0 ? <span className='today'>Cьогодні</span> : null}
-                        <span className="day-number">{arrNameWeek[index + 1] }</span>
-                        <h3>{new Date(dayData.date_epoch * 1000).getDate()}</h3>
-                        <img src={dayData.day.condition.icon || 'https://i.gifer.com/VAyR.gif'} alt="Image weather" />
+                        <span className="day-week">{arrNameWeek[index + 1]}</span>
+                        <h3 className='day-month'>{new Date(dayData.date_epoch * 1000).getDate()}</h3>
+                        <img src={dayData.day.condition.icon} alt="Image weather" />
                         <div className="temp-day">{dayData.day.avgtemp_c}&deg;C</div>
-                        <div className='temperature'>
+                        <div>
                             <div className="degrees-celsius">
                                 <div className="degrees-min">Min<br /><span>{dayData.day.mintemp_c}</span><sup>&deg;</sup></div>
                                 <div className="degrees-max">Max<br /><span>{dayData.day.maxtemp_c}</span><sup>&deg;</sup></div>
--- src/context/weatherContext.jsx
+++ src/context/weatherContext.jsx
@@ -9,7 +9,7 @@ export const WeatherProvider = ({ children }) => {
     useEffect(() => {
         async function loadData() {
             try {
-                const weatherApi = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=52d9f961032045a097064443231911&q=${nameCity}&days=5&lang=uk`)
+                const weatherApi = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=52d9f961032045a097064443231911&q=${nameCity}&days=7&lang=uk`)
                 const resp = await weatherApi.json()
                 setData(resp)
             } catch (error) {
