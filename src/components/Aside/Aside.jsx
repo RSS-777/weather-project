cd98d84f1be7da21d6e653f27fd2521d841b7c95
Sergey_js15 <serj.777.front@gmail.com>
2023-12-12 22:13:07 +0200

    add advertising

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -13,8 +13,16 @@ export const Aside = () => {
 
     return (
         <aside className={theme === 'white' ? `${stateSeason}` : 'aside-dark'}>
-            <div className="advertising">Тут може бути ваша реклама!</div>
-            <div className="advertising">Тут може бути ваша реклама!</div>
+            <div className="advertising">
+                <a href="https://fashiontema.com.ua/dostupno-yakisno-ta-stylno-zhinochyi-odiah-vyrobnyka-feshentema/" target="_blank">
+                    <img  src="https://fashiontema.com.ua/content/uploads/images/zhnochiystilni-gol.png" alt="Реклама магазин одягу" />
+                </a>
+            </div>
+            <div className="advertising">
+                <a href="https://rozetka.com.ua/news-articles-promotions/promotions/233522_sale_bosch/" target="_blank">
+                    <img  src="https://content.rozetka.com.ua/promotions/main_image/original/388741919.jpg" alt="Реклама розетка" />
+                </a>
+            </div>
             <div className="advertising">Тут може бути ваша реклама!</div>
         </aside>
     )
